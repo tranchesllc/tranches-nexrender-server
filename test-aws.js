83d280278a7d2bb0185bd5db0843bd99c204fd35
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-07 10:06:39 +0800

    fix format

--- test-aws.js
+++ test-aws.js
@@ -1,19 +1,19 @@
 const { S3Client, ListBucketsCommand } = require("@aws-sdk/client-s3");
 
-const s3Client = new S3Client({ 
-  region: process.env.AWS_REGION, 
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
 });
 
 const run = async () => {
-  try {
-    const data = await s3Client.send(new ListBucketsCommand({}));
-    console.log("Success", data.Buckets);
-  } catch (err) {
-    console.log("Error", err);
-  }
+    try {
+        const data = await s3Client.send(new ListBucketsCommand({}));
+        console.log("Success", data.Buckets);
+    } catch (err) {
+        console.log("Error", err);
+    }
 };
 run();
