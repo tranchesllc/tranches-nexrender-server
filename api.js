0d333e57f7ec59de46b01164cd267cea62f2af8d
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-12-04 22:04:25 +0800

    change host

--- api.js
+++ api.js
@@ -149,6 +149,6 @@ app.delete("/jobs/:id", async (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`API server running on port ${port}`);
 });
